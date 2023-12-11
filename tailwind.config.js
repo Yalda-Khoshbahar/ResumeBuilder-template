ef0e90675a90ffe7f7c8d4b4f80d8cdacb33fe69
Yalda Khoshbahar <y.khoshbahar2003@gmail.com>
2023-12-11 15:48:00 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily : {
+        quicksand: ['"Quicksand"', ...defaultTheme.fontFamily.sans]
+      },
+      screens: {
+        '375': '375px',
+        // => @media (min-width: 375px) { ... }
+        '425': '425px',
+        // => @media (min-width: 425px) { ... }
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+      colors : {
+        'primary' : '#FF5C00'
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+    plugins: [],
+  }
+}
