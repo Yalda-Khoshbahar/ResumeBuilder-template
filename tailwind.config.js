c24c5af321ac2c85cce7a16a8b6048ec15263172
Yalda Khoshbahar <y.khoshbahar2003@gmail.com>
2024-01-15 14:26:39 +0330

    Update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./**/*.html"],
   theme: {
     extend: {
       fontFamily : {
